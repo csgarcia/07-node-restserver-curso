5a195ec645f7639a6ca8c0d24abcde55e04f8951
Carlos <carlossalgt@gmail.com>
2020-05-31 14:36:02 -0600

    Env var for db

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // Enviroment //
 let urlDb = 'mongodb://localhost:27017/cafe';
 if (process.env.NODE_ENV !== 'dev') {
-    urlDb = 'mongodb://carlos:123456-a@ds259109.mlab.com:59109/cafe'
+    urlDb = process.env.MONGO_URI
 }
 process.env.URL_DB = urlDb;
 // ===================== //
